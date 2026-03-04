259d328b19432aa8de00d6872974cf7bf7391634
shadow-Sx <shadowsoya8@gmail.com>
2026-03-05 01:28:50 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,10 @@
+from pymongo import MongoClient
+from config import MONGO_URI
 
+client = MongoClient(MONGO_URI)
+db = client["xanimelar_bot"]
+
+contents = db["contents"]
+required_channels = db["required_channels"]
+optional_channels = db["optional_channels"]
+admins = db["admins"]
