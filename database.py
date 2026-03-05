d5c606ad451208b7e6dc1cbafde07df2882d9501
shadow-Sx <shadowsoya8@gmail.com>
2026-03-05 04:10:24 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,27 +1,12 @@
 from pymongo import MongoClient
 from config import MONGO_URI
 
-# ==========================
-#   MONGO DB CONNECTION
-# ==========================
-
 client = MongoClient(MONGO_URI)
+db = client["bot"]
 
-# Asosiy database
-db = client["xanimelar_bot"]
-
-# ==========================
-#   COLLECTIONS
-# ==========================
-
-# Kontentlar (video, photo, document, text)
-contents = db["contents"]
-
-# Majburiy obuna kanallari
+users = db["users"]
 required_channels = db["required_channels"]
-
-# Ixtiyoriy obuna kanallari
 optional_channels = db["optional_channels"]
-
-# Adminlar (hozircha ishlatilmaydi, lekin kelajakda kerak bo‘ladi)
-admins = db["admins"]
+referrals = db["referrals"]
+settings = db["settings"]
+broadcast = db["broadcast"]
