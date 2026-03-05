19d0077ab5bfa15879e14b088bb4be277d6aea02
shadow-Sx <shadowsoya8@gmail.com>
2026-03-05 08:29:38 +0300

    Create bot_handlers_2.py

---
+++ bot_handlers_2.py
@@ -0,0 +1,6 @@
+from bot_handlers_1 import bot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+from utils import (
+    BOT_USERNAME, ADMIN_ID,
+    contents, required_channels, optional_channels,
+    referrals, settings,
